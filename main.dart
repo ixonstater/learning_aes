34c535ee92208cbe4bfd417ca94b0cd843b67f56
josh <joshuajarvis0711@gmail.com>
2021-04-09 07:34:10 -0600

    Added benchmarking.

--- main.dart
+++ main.dart
@@ -1,10 +1,19 @@
 import 'lib/aes.dart';
 
 void main(List<String> args) {
-  String msg = "a4a4a4a4";
+  String msg = "";
+  for (var i = 0; i < 100000; i++) {
+    msg += "A";
+  }
   var key = new AesSymmetricKey();
   var encryptor = new AesEncrypt(key);
+
+  var start = DateTime.now();
   var encryptedString = encryptor.encrypt(msg);
+  var end = DateTime.now();
+  var interval = end.difference(start).inMilliseconds;
+
   print("Encryption result: " + encryptedString);
   print("Encryption key: " + key.toString());
+  print("Encryption took: " + interval.toString());
 }
