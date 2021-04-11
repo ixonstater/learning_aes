c2e979a542c75ebc465cef2aea63a3998ac718dd
josh <joshuajarvis0711@gmail.com>
2021-04-11 12:31:10 -0600

    Finished key expansion explanation.

--- main.dart
+++ main.dart
@@ -1,20 +1,16 @@
 import 'lib/aes.dart';
 
 void main(List<String> args) {
-  // String msg = "hello";
-  // var key = new AesSymmetricKey();
-  // var encryptor = new AesEncrypt(key);
+  String msg = "hello";
+  var key = new AesSymmetricKey();
+  var encryptor = new AesEncrypt(key);
 
-  // var start = DateTime.now();
-  // var encryptedString = encryptor.encrypt(msg);
-  // var end = DateTime.now();
-  // var interval = end.difference(start).inMilliseconds;
+  var start = DateTime.now();
+  var encryptedString = encryptor.encrypt(msg);
+  var end = DateTime.now();
+  var interval = end.difference(start).inMilliseconds;
 
-  // print("Encryption result: " + encryptedString);
-  // print("Encryption key: " + key.toString());
-  // print("Encryption took: " + interval.toString());
-  //
-  var sbox = new Sbox();
-  print("");
-  sbox.printSbox();
+  print("Encryption result: " + encryptedString);
+  print("Encryption key: " + key.toString());
+  print("Encryption took: " + interval.toString());
 }
