32a694a826614dbf3be70efecccb370547979d8e
josh <joshuajarvis0711@gmail.com>
2021-04-12 11:13:36 -0600

    Fixed issue with blog example key expansion, fixed issue with example key byte order.

--- main.dart
+++ main.dart
@@ -1,10 +1,10 @@
 import 'lib/aes.dart';
 
 void main(List<String> args) {
-  var key = new AesSymmetricKey();
+  var key = new AesSymmetricKey.blogExampleKey();
   var encryptor = new AesEncrypt(key);
 
-  var encryptedString = encryptor.encrypt("");
+  var encryptedString = encryptor.blogExample();
 
   print("Encryption result: " + encryptedString);
   print("Encryption key: " + key.toString());
